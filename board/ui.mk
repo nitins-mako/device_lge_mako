LineageOS/android_device_sony_huashan

libui: Allow additional gralloc 1.0 buffer usage bits
 * Our platform requires 0x02000000U,
    which appears to be USAGE_PRIVATE_NONSECURE

 * Resolves the following Gralloc2 failure upon playback:
    buffer descriptor contains invalid usage bits 0x2000000

Change-Id: Ie5a8a46fef1c0f28ebd2cd3b3d29cc19763bc2a3
Signed-off-by: Adrian DC <radian.dc@gmail.com>

board/ui.mk
@@ -1,0 +1,2 @@
+# UI
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x02000000U

nitins-mako/device_lge_mako

 * Our platform requires 0x02002000U,
    buffer descriptor contains invalid usage bits 0x2002000

board/ui.mk
@@ -1,0 +1,2 @@
+# UI
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x02002000U 