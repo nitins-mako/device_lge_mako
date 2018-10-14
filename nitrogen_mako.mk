2b5672f7e551f9ee3219b0fea24bbb03bd45b415
nitin1438 <nitin.chobhe@gmail.com>
2018-10-14 18:28:57 +0530

    Add build fingerprint

--- nitrogen_mako.mk
+++ nitrogen_mako.mk
@@ -12,9 +12,16 @@ $(call inherit-product, vendor/nitrogen/products/common.mk)
 $(call inherit-product, device/lge/mako/full_mako.mk)
 $(call inherit-product, device/lge/mako/board/ui.mk)
 
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
 PRODUCT_NAME := nitrogen_mako
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
+
+# Device build info and fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=occam \
+    PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48T 2237560 release-keys"
+
+BUILD_FINGERPRINT := google/occam/mako:5.1.1/LMY48T/2237560:user/release-keys
